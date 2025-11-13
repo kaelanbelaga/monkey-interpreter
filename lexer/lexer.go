9b582f1856d68348c1a96413f78e2e56e8f0cd55
Kaelan Belaga <kaelan@belaga.net>
2025-11-13 18:29:11 +0100

    feat: add functionality to parse numbers to lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -56,6 +56,10 @@ func (l *Lexer) NextToken() token.Token {
 			tok.Literal = l.readIdentifier()
 			tok.Type = token.LookupIdent(tok.Literal)
 			return tok
+		} else if isDigit(l.ch) {
+			tok.Literal = l.readNumber()
+			tok.Type = token.INT
+			return tok
 		} else {
 			tok = newToken(token.ILLEGAL, l.ch)
 		}
@@ -73,6 +77,10 @@ func isLetter(ch byte) bool {
 	return 'a' <= ch && ch <= 'z' || 'A' <= ch  && ch <= 'Z' || ch == '_'
 }
 
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
 func (l *Lexer) skipWhitespace() {
 	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
 		l.readChar()
@@ -87,3 +95,10 @@ func (l *Lexer) readIdentifier() string {
 	return l.input[position:l.position]
 }
 
+func (l *Lexer) readNumber() string {
+ position := l.position
+ for isDigit(l.ch) {
+	 l.readChar()
+ }
+	return l.input[position:l.position]
+}
