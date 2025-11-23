6a8fe1625ce4e71bedce670b0b30dd5ce495a3f6
Kaelan Belaga <kaelan@belaga.net>
2025-11-23 20:06:26 +0100

    feat: add simple error handling to parser

--- parser/parser.go
+++ parser/parser.go
@@ -2,20 +2,23 @@
 package parser
 
 import (
+	"fmt"
+
 	"monkey/ast"
 	"monkey/lexer"
 	"monkey/token"
 )
 
 type Parser struct {
-	l *lexer.Lexer
+	l      *lexer.Lexer
+	errors []string
 
 	curToken  token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{l: l, errors: []string{}}
 
 	// Read 2 tokens for peekToken and curToken to be set
 	p.nextToken()
@@ -87,6 +90,18 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+
+	p.errors = append(p.errors, msg)
 }
